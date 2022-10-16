7930c0148c597f84a227b567e6c4019ae65624fa
Melvin Lee <melvin.lee@iswish.nl>
2022-10-16 15:14:30 +0200

    feat(productcatalog.webapi): added serilog

---
+++ src/ProductCatalog.WebApi/LogHelper.cs
@@ -0,0 +1,28 @@
+﻿using Serilog.Events;
+
+namespace ProductCatalog.WebApi;
+
+public static class LogHelper
+{
+    private static bool IsHealthCheckEndpoint(HttpContext ctx)
+    {
+        var endpoint = ctx.GetEndpoint();
+
+        if (endpoint is null) return false;
+
+        return string.Compare(endpoint.DisplayName,
+                              "Health checks",
+                              StringComparison.Ordinal) == 0;
+    }
+
+    private static bool IsServerError(HttpContext ctx, Exception ex) => ex is not null || ctx.Response.StatusCode > 499;
+
+    public static LogEventLevel LogHealthCheckRequestsWithDebugEventLevel(HttpContext ctx, double _, Exception ex)
+    {
+        if (IsServerError(ctx, ex)) return LogEventLevel.Error;
+
+        if (IsHealthCheckEndpoint(ctx)) return LogEventLevel.Debug;
+
+        return LogEventLevel.Information;
+    }
+}
--- src/ProductCatalog.WebApi/Program.cs
+++ src/ProductCatalog.WebApi/Program.cs
@@ -1,4 +1,23 @@
+using ProductCatalog.WebApi;
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Sinks.SystemConsole.Themes;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Host.UseSerilog((context, config) =>
+{
+    config
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.FromLogContext()
+        //.Enrich.WithExceptionDetails()
+        .Enrich.With<ActivityEnricher>()
+        .WriteTo.Console(
+            outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+            theme: AnsiConsoleTheme.Code)
+        ;
+});
 
 // Add services to the container.
 builder.Services.AddHealthChecks();
@@ -7,6 +26,11 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseSerilogRequestLogging(options =>
+{
+    options.GetLevel = LogHelper.LogHealthCheckRequestsWithDebugEventLevel;
+});
 
 app.UseAuthorization();
 
